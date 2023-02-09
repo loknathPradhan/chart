6b815259846607582b2aa851c86482e29e1f1d77
loknath pradhan <loknath11a@gmail.com>
2023-02-09 16:12:39 +0530

    added a line graph to represent subscriber in a year

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from "react-chartjs-2"
+import { useEffect, useState } from 'react';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement
+)
 
 function App() {
+
+  const [data, setData] = useState({
+    datasets: []
+  });
+  const [option, setOption] = useState();
+
+  useEffect(() => {
+    setData({
+      labels: ["2010", "2012", "2014", "2016", "2018", "2020"],
+      datasets: [
+        {
+          label: "Subscriber in a year",
+          data: [50, 10, 100, 40, 70, 20],
+          borderColor: "rgb(53,162,235)",
+          backgroundColor: "rgba(53,162,235)",
+        }
+      ]
+    });
+    setOption({
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      Plugin: {
+        legend: {
+          position: "top"
+        },
+        title: {
+          display: true,
+          text: "voting"
+        },
+      },
+    })
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+      <Line options={option} data={data} />
     </div>
   );
 }
-
 export default App;
